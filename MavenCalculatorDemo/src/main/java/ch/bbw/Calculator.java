e52c2b89f86c8a9c37d2470139504c2b7f8da968
FC081025 <FC081025@192.168.239.1>
2017-01-09 00:05:00 +0100

    Comment was edited

--- MavenCalculatorDemo/src/main/java/ch/bbw/Calculator.java
+++ MavenCalculatorDemo/src/main/java/ch/bbw/Calculator.java
@@ -1,7 +1,7 @@
 package ch.bbw;
 
 public class Calculator {
-	//Method for adding two nums
+	//Method for adding two nums 
 	public int addition (int summand1, int summand2){
 		int result = 0;
 		result = summand1 + summand2;
